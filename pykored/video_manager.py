1ad329301ae96586cf7d9f91d18bad40cae279ae
pykored <dev5027322@gmail.com>
2024-12-15 17:34:00 +0900

    Update video_manager.py

--- pykored/video_manager.py
+++ pykored/video_manager.py
@@ -1,6 +1,7 @@
 import os
 import re
 import shutil
+from tqdm import tqdm 
 from pykored.downloader import VideoDownloader
 
 class VideoManager:
@@ -10,10 +11,20 @@ class VideoManager:
 
     async def download_video(self, segment_urls, title):
         output_filename = re.sub(r'[:*?"<>|/\\]', '_', f'{title}.mp4')
-        await self.downloader.download_segments_with_asyncio(segment_urls)
+        total_segments = len(segment_urls)
+
+        with tqdm(total=total_segments, desc="Downloading", unit="segment") as pbar:
+            await self.downloader.download_segments_with_asyncio(
+                segment_urls, progress_callback=lambda: pbar.update(1)
+            )
+        
+        print("\nDownload complete!")
+        
         self.downloader.merge_ts_files(output_filename)
         output_mp4_path = os.path.join(self.output_mp4_dir, output_filename)
         os.rename(os.path.join(self.downloader.output_dir, output_filename), output_mp4_path)
+        
+        print(f"Video saved as: {output_mp4_path}")
         self.delete_video_segments_folder(self.downloader.output_dir)
 
     def delete_video_segments_folder(self, output_dir):
